d2f340787f497a5394c922c533ac6b767320ab5e
Mika Jihoon Yoon <mika@zoyi.co>
2018-07-06 16:55:53 +0900

    init

---
+++ src/main/java/ca/frederi/version/Version.java
@@ -0,0 +1,14 @@
+package ca.frederi.version;
+
+public class Version {
+
+  private static String version = "0.0.1";
+
+  public static String getVersion() {
+    return version;
+  }
+
+  public static String getA() {
+    return "A";
+  }
+}
